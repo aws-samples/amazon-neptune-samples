a0290886b25ec18235e1f0b74b6264795211f5b0
Vikrama Adethyaa <adethyaa@gmail.com>
2018-04-17 10:05:51 +0530

    corrected local variable 'offer_term_vertex' scope

--- aws_ec2_instance_prices.py
+++ aws_ec2_instance_prices.py
@@ -56,7 +56,7 @@ def add_product_priceTerm_and_edge(graph_traversal, offer_type, aws_region_code,
             gremlin_interface.add_update_vertex_properties(graph_traversal,
                                                            offer_term_vertex,
                                                            offer_term_vertex_attributes)
-        elif offer_term_vertex == 1:
+        elif len(offer_term_vertex_list) == 1:
             offer_term_vertex = offer_term_vertex_list[0]
 
         # Create Edge from AWS-Region to Offer-Term
