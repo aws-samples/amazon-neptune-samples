6e5b8cd853fac6dbd767382fdcb15d746b9cf12f
Jaypee <jignacio@cambridge.org>
2020-03-02 12:31:19 +0800

    Refactor lambda code

--- gremlin/visjs-neptune/indexLambda.js
+++ gremlin/visjs-neptune/indexLambda.js
@@ -20,153 +20,78 @@
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
-**/
+* */
 
-var gremlin = require('gremlin');
-var http = require('http');
-var url = require('url');
+const gremlin = require('gremlin');
 
-exports.outnodes = [];
-
-exports.handler = function(event, context, callback) {
-
-    var DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
-    var Graph = gremlin.structure.Graph;
-    //Use wss:// for secure connections. See https://docs.aws.amazon.com/neptune/latest/userguide/access-graph-ssl.html 
-    dc = new DriverRemoteConnection(
-      `wss://${process.env.NEPTUNE_CLUSTER_ENDPOINT}:${process.env.NEPTUNE_PORT}/gremlin`,
-      {mimeType: 'application/vnd.gremlin-v2.0+json'}
-    );
-
-    var graph = new Graph();
-    var g = graph.traversal().withRemote(dc);
-
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Max-Age': 2592000, // 30 days
-        /** add other headers as per requirement */
-        'Access-Control-Allow-Headers' : '*',
-        "Content-Type": "application/json"
-    };
-
-    console.log("Path Parameters => "+ event.pathParameters);
-    console.log("event.pathParameters.proxy => "+ event.pathParameters.proxy);
-    console.log(event.pathParameters.proxy.match(/proxy/ig));
-
-    // this code is only for populating the search LoV
-    if (event.pathParameters.proxy.match(/initialize/ig)) {
-        //using another technique as opposed to creating a new callback function
-
-        g.V().hasLabel('User').limit(1000).valueMap(true).toList().then(
-            data => {
-            console.log("Response from Neptune for initialize .." + JSON.stringify(data));
-        var nodes=[];
-        for(var i = 0;    i < data.length;    i++)
-        {
-            nodes.push({name: data[i].name.toString()});
-        }
-        var response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(nodes)
-        };
-        console.log("Initialize call response: " + JSON.stringify(data));
-        callback(null, response);
-        context.done();
-        dc.close(); // look at this carefully!!!
-    }).
-        catch(error => {
-            console.log('ERROR', error);
-        dc.close();
-    });
-    }
-
-
-    if (event.pathParameters.proxy.match(/search/ig)) {
-        g.V().has('name', gremlin.process.P.between(event.queryStringParameters.username, event.queryStringParameters.touser)).limit(20).valueMap(true).toList().then(
-            data => {
-            console.log(JSON.stringify(data));
-            var response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(data)
-        };
-        console.log("Search call response: " + JSON.stringify(data));
-        callback(null, response);
-        context.done();
-        dc.close(); // look at this carefully!!!
-    }).
-        catch(error => {
-            console.log('ERROR', error);
-        dc.close();
-    });
-    }
-
-
-    if (event.pathParameters.proxy.match(/neighbours/ig)) {
-        g.V().has('User','~id',event.queryStringParameters.id).in_('Follows').valueMap(true).limit(10).toList().then(
-            data => {
-            console.log(JSON.stringify(data));
-        var response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(data)
-        };
-        console.log("getNeighbours response: " + JSON.stringify(data));
-        callback(null, response);
-        context.done();
-        dc.close();
-    }).
-        catch(error => {
-            console.log('ERROR', error);
-        dc.close();
-    });
-
-    }
-
-
-    if (event.pathParameters.proxy.match(/getusertweets/ig)) {
-        g.V().has('User', '~id', event.queryStringParameters.userid).out('Tweets').limit(3).valueMap(true).toList().then(
-            data => {
-        console.log("getusertweets data" + JSON.stringify(data));
-        var response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(data)
-        };
-        console.log("getusertweets response: " + JSON.stringify(data));
-        callback(null, response);
-        context.done();
-        dc.close(); // look at this carefully!!!
-        }).
-        catch(error => {
-            console.log('ERROR', error);
-        dc.close();
-        });
-    }
-
-
-    if (event.pathParameters.proxy.match(/whichusersliketweet/ig)) {
-        g.V().has('Tweet','~id',event.queryStringParameters.tweetid).in_('Likes').hasLabel('User').limit(5).valueMap(true).toList().then(
-            data => {
-            console.log(JSON.stringify(data));
-        var response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(data)
-        };
-        console.log("getusertweets response: " + JSON.stringify(data));
-        callback(null, response);
-        context.done();
-        dc.close(); // look at this carefully!!!
-    }).
-        catch(error => {
-            console.log('ERROR', error);
-        dc.close();
-    });
-    }
-
-
+const formatResponse = payload => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+      'Access-Control-Max-Age': 2592000, // 30 days
+      'Access-Control-Allow-Headers': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  };
 }
 
+exports.handler = async event => {
+  const {DriverRemoteConnection} = gremlin.driver;
+  const {Graph} = gremlin.structure;
+  // Use wss:// for secure connections. See https://docs.aws.amazon.com/neptune/latest/userguide/access-graph-ssl.html
+  const dc = new DriverRemoteConnection(
+    `wss://${process.env.NEPTUNE_CLUSTER_ENDPOINT}:${process.env.NEPTUNE_PORT}/gremlin`,
+    {mimeType: 'application/vnd.gremlin-v2.0+json'}
+  );
+  const graph = new Graph();
+  const g = graph.traversal().withRemote(dc);
+
+  try {
+    let data = [];
+    const {
+      id, username, touser, userid, tweetid
+    } = event.queryStringParameters || {};
+
+    if (event.pathParameters.proxy.match(/initialize/ig)) {
+      const nodes = await g.V()
+        .hasLabel('User')
+        .limit(1000)
+        .valueMap(true)
+        .toList();
+      data = nodes.map(row => ({name: row.name.toString()}));
+    } else if (event.pathParameters.proxy.match(/search/ig)) {
+      data = await g.V().has('name', gremlin.process.P.between(username, touser))
+        .limit(20)
+        .valueMap(true)
+        .toList();
+    } else if (event.pathParameters.proxy.match(/neighbours/ig)) {
+      data = await g.V().has('User', '~id', id)
+        .in_('Follows')
+        .valueMap(true)
+        .limit(10)
+        .toList();
+    } else if (event.pathParameters.proxy.match(/getusertweets/ig)) {
+      data = await g.V().has('User', '~id', userid)
+        .out('Tweets')
+        .limit(3)
+        .valueMap(true)
+        .toList();
+    } else if (event.pathParameters.proxy.match(/whichusersliketweet/ig)) {
+      data = await g.V().has('Tweet', '~id', tweetid)
+        .in_('Likes')
+        .hasLabel('User')
+        .limit(5)
+        .valueMap(true)
+        .toList();
+    }
+
+    dc.close();
+    return formatResponse(data);
+  } catch (error) {
+    console.log('ERROR', error);
+    dc.close();
+  }
+};
