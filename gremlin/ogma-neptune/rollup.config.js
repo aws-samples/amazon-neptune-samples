773bbc5a08e9826a3dbc5148cac18562f21f7061
Alexander Milevski <alexander@linkurio.us>
2022-04-28 16:02:28 +0200

    Moved js files to 'src'

--- gremlin/ogma-neptune/rollup.config.js
+++ gremlin/ogma-neptune/rollup.config.js
@@ -1,7 +1,7 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
-  input: "./index.js",
+  input: "./src/index.js",
   output: {
     file: "./dist/index-bundle.js",
     format: "iife",
